adf199b85cb6cf00da817d5a38346a2b5ac120fd
Atharva <atharvapokharkar2005@gmail.com>
2023-10-19 19:33:51 +0530

    Basic completed

---
+++ Day_1 of Python/Printing.py
@@ -0,0 +1,12 @@
+print("Hello World!!")
+
+#Exercise:
+print("Day 1 - Python Print Function")
+print("The Function is declared like this:")
+print("print('What to print')") 
+
+#If we want double quotes inside we need to follow the following syntax:
+print('print("What to print")') 
+
+#To print on next line we can use the following syntax:
+print("Hello World!\nHello World!\nHello world!")
---
+++ Day_1 of Python/Strings.py
@@ -0,0 +1,17 @@
+#Anything placed inside double quotes or single quotes is called as string.
+print("My name is Hacker")
+
+#String Concatenation:(addition of two strings)
+print("Hello "+"Anon")
+#or
+print("Hello"+" "+"Anon")
+
+#There are three types of errors in python:
+#1)Syntax Error : causes due to invalid syntax.
+#2)Indentation error : causes to wrong indentation mainly due to wide spaces.
+#3)Runtime error : This error will not give you the desired output.
+
+#String manipulation exercise:
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello" + "World")\nNew lines can be created with backslash and n.')
