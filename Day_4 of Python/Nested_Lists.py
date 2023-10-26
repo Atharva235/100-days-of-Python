cc2b6162dda534d9afc6e9ce8700fc15d17906a2
Atharva <atharvapokharkar2005@gmail.com>
2023-10-26 17:14:52 +0530

    Day 4 completed

---
+++ Day_4 of Python/Nested_Lists.py
@@ -0,0 +1,6 @@
+# dirty_dozen=["Strawberries","spinach","Kale","Apples","Peaches","Cheeries","Tomatoes","Potatoes","Pears"]
+
+fruits=["Strawberries","Apples","Peaches","Cheeries","Pears"]
+vegetables=["spinach","Tomatoes","Potatoes","Kale"]
+dirty_dozen=[fruits,vegetables]
+print(dirty_dozen)
